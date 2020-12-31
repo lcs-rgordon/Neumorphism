fd7d1b37a9a95d496a1bf477dcfc1519f9bb4b35
Russell Gordon <github@russellgordon.ca>
2020-12-31 15:35:35 -0500

    Button now shows taps visually, but effect disappears when tapped.  🤔

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -46,10 +46,17 @@ struct SimpleButtonStyle: ButtonStyle {
         configuration.label
             .padding(30)
             .background(
-                Circle()
-                    .fill(Color.offWhite)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                Group {
+                    if configuration.isPressed {
+                        Circle()
+                            .fill(Color.offWhite)
+                    } else {
+                        Circle()
+                            .fill(Color.offWhite)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                            .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                    }
+                }
             )
         
     }
