60254e72c637551f0bfa6c781a071e10f7f10a76
Russell Gordon <github@russellgordon.ca>
2020-12-31 15:26:56 -0500

    Simple card – easy!

---
+++ Neumorphism/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Neumorphism
+//
+//  Created by Russell Gordon on 2020-12-31.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let offWhite = Color(red: 225 / 255, green: 225 / 255, blue: 235 / 255)
+    
+}
--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.offWhite
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.offWhite)
+                .frame(width: 300, height: 300)
+                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+
+        }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
