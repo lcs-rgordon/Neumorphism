475da68f284ab77378374692e57aafc9b8afa795
Russell Gordon <github@russellgordon.ca>
2020-12-31 15:33:00 -0500

    Added a circular button, but tapping it shows nothing visual, which is weird.

--- Neumorphism/ContentView.swift
+++ Neumorphism/ContentView.swift
@@ -11,14 +11,47 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color.offWhite
-            RoundedRectangle(cornerRadius: 25)
-                .fill(Color.offWhite)
-                .frame(width: 300, height: 300)
-                .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            
+            VStack {
+                Spacer()
+                
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.offWhite)
+                    .frame(width: 300, height: 300)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+                
+                Spacer()
+                
+                Button(action: {
+                    print("Button tapped")
+                }, label: {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.gray)
+                })
+                .buttonStyle(SimpleButtonStyle())
+                
+                Spacer()
+
+            }
+            
 
         }
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct SimpleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(30)
+            .background(
+                Circle()
+                    .fill(Color.offWhite)
+                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
+                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
+            )
+        
     }
 }
 
