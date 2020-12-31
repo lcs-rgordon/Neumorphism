3d57c34aa7e16847066a3ed2fd1adc6316b838be
Russell Gordon <github@russellgordon.ca>
2020-12-31 15:18:27 -0500

    Initial Commit

---
+++ Neumorphism/NeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismApp.swift
+//  Neumorphism
+//
+//  Created by Russell Gordon on 2020-12-31.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
